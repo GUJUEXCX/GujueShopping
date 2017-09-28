20046d71c97822d23364e4b4fea86d9268f02812
maxutian <maxutian95@gmail.com>
2017-09-28 18:11:06 +0800

    'categoryPage'

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,10 +1,22 @@
 var app = getApp()
-
 Page({
   data: {
+    navLeftItems: [],
+    navRightItems: [],
+    curNav: 1,
+    curIndex: 0
   },
   onLoad: function () {
   },
-  onShow: function () {
-  },
+
+  // 事件处理函数
+  switchRightTab: function (e) {
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    this.setData({
+      curNav: id,
+      curIndex: index
+    })
+  }
+
 })
